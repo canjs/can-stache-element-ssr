0a558c4c40ebff4a96d238c446eeb2b0d2a907ff
Mark Thompson <mthompson@bitovi.com>
2022-10-25 15:35:41 -0700

    sample code for worker threads instead of process spawns

--- jsdom-ssr/index.js
+++ jsdom-ssr/index.js
@@ -1,4 +1,8 @@
+const pLimit = require("p-limit")
+
 const spawnBuildProcess = require("./spawn-build-process")
+const workerThread = require("./worker-thread")
+
 const { ensureDir, emptyDir, readJson } = require("fs-extra")
 const stealTools = require("steal-tools")
 const argv = require("optimist").argv
@@ -24,11 +28,27 @@ async function main() {
   await emptyDir("dist/ssr")
 
   // Read paths to generate static pages
-  const ssgSettings = await readJson("ssg.json")
+  const ssgSettings = await readJson("ssg-max.json")
 
   const routes = ssgSettings.routes
 
+  // const limit = pLimit(40);
+
+  // const pool = routes.map(route => limit(function(){
+  //   // return spawnBuildProcess(route, !!argv.prod)
+  //   return workerThread(route,!!argv.prod)
+  // }));
+
+  // const completed = await Promise.all(pool);
+
+  // console.log(completed.length);
+
   for (const route of routes) {
-    spawnBuildProcess(route, !!argv.prod)
+    // spawnBuildProcess(route, !!argv.prod)
+    workerThread(route, !!argv.prod)
   }
 }
+
+// Time things:
+// npm i -D gnomon
+// npm run build | gnomon
