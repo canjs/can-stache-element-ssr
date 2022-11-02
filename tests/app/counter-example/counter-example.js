833d7be4033f04dad7cc27d60e91190aa489fde0
Mark Thompson <mthompson@bitovi.com>
2022-11-01 18:25:56 -0700

    minor formatting

--- tests/app/counter-example/counter-example.js
+++ tests/app/counter-example/counter-example.js
@@ -3,9 +3,9 @@ import { ssgDefineElement } from "../../../client-helpers/ssg-helpers.js"
 
 class CounterExample extends StacheElement {
   static view = `
-        <p data-testid="counter">{{ this.count }}</p>
-        <button data-testid="counter-button" on:click='this.increment()'>+1</button>
-    `
+    <p data-testid="counter">{{ this.count }}</p>
+    <button data-testid="counter-button" on:click='this.increment()'>+1</button>
+  `
 
   static props = {
     count: 0,
