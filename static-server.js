06a9c9655470714878cd3f853d8d832fe145f28f
Mark Thompson <mthompson@bitovi.com>
2022-11-02 20:45:51 -0700

    added static-server to test if hosting ssg works as expected

--- static-server.js
+++ static-server.js
@@ -1,18 +1,34 @@
 const express = require("express")
 const path = require("path")
-// const { existsSync } = require("fs-extra")
-// const { getEnvironments, getEnvConfiguration } = require("./client-helpers/environment-helpers")
 const argv = require("optimist").argv
+const { getEnvConfiguration } = require("./client-helpers/environment-helpers")
+const environment = require("./jsdom-ssg/flags/get-ssg-environment")()
+
 const app = express()
-// const defaultEnvironment = require("./jsdom-ssg/flags/get-ssg-environment")()
-// const serverMode = require("./jsdom-ssg/flags/get-server-mode")()
+const envConfiguration = getEnvConfiguration(environment)
+const basePath = path.join("dist", envConfiguration.dist.basePath)
 
-app.use(express.static(path.join(__dirname, "dist/prod/static")))
-// app.use('*', express.static(path.join(__dirname, 'dist/prod/static/404')));
+/**
+ * This fix requires a change in `can-route`:
+ * 
+  node_modules/can-route/src/deparam.js
+  ```javascript
+  function canRoute_deparam(url) {
+    url = toURLFragment(url);
+    console.log('canRoute_deparam', url)// progressive-loading/moo/
+    if (url.charAt(url.length - 1) === '/') {
+      url = url.slice(0, -1)// Temp fix, remove trailing "/"
+    }
+    console.log('canRoute_deparam', url)// progressive-loading/moo
+  }
+  ```
 
-// app.use(express.static(path.join(__dirname, 'test')));
-// app.use('*', express.static(path.join(__dirname, 'test/404')));
+  Don't forget to rebuild after you make change in `node_modules`
+ */
+
+app.use(express.static(path.join(__dirname, basePath, envConfiguration.dist.static)))
+app.use("*", express.static(path.join(__dirname, basePath, envConfiguration.dist.static, "404")))
 
 app.listen(argv.port || 8080, function () {
-  console.log("Example app listening on port " + (argv.port || 8080))
+  console.log("Example app listening on port ", argv.port || 8080)
 })
